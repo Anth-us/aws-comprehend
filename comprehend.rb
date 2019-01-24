ee12f54cc8dc47bbbccfee55e28abb1cbc81b936
Ryan Alyn Porter <rap@endymion.com>
2019-01-24 20:45:39 +0000

    Added detect_key_phrases.
    https://gist.github.com/endymion/60aade9fd77d609865a5045a87dc83a3

--- comprehend.rb
+++ comprehend.rb
@@ -12,8 +12,15 @@ program :version, '0.0.1'
 program :description, 'Use AWS Comprehend for natural language processing.'
 
 command :detect_entities do |c|
-  c.syntax = 'comprehend detect_entities [options]'
+  c.syntax = 'comprehend detect_entities [message]'
   c.action do |args, options|
     ap AWSComprehend.new.detect_entities(args[0])
+  end
+end
+
+command :detect_key_phrases do |c|
+  c.syntax = 'comprehend detect_key_phrases [message]'
+  c.action do |args, options|
+    ap AWSComprehend.new.detect_key_phrases(args[0])
   end
 end
--- lib/aws-comprehend.rb
+++ lib/aws-comprehend.rb
@@ -4,10 +4,18 @@ class AWSComprehend
   end
   
   def detect_entities(message, language: 'en')
-    puts "Analying: \"#{message}\"..." if $verbose
+    puts "detect_entities in: \"#{message}\"..." if $verbose
     @client.detect_entities({
       text: message,
       language_code: language
     }).entities
+  end
+
+  def detect_key_phrases(message, language: 'en')
+    puts "Analying: \"#{message}\"..." if $verbose
+    @client.detect_key_phrases({
+      text: message,
+      language_code: language
+    }).key_phrases
   end
 end
